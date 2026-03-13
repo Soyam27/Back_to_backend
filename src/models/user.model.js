1e421bda341bfc87a8d2a50b8509e1d88af2bcc2
Soyam Paul <soyampaul34@gmail.com>
2026-03-13 06:52:36 +0530

    added models and custom methods to userschema and plugins in videomodel

---
+++ src/models/user.model.js
@@ -0,0 +1,76 @@
+import mongoose, { Schema } from 'mongoose';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+    },
+    fullName: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true,
+    },
+    avatar: {
+        type: String,
+        required: true,
+    },
+    coverImage: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    watchHistory: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Video'
+        }
+    ]
+}, { timestamps: true });
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+})
+
+userSchema.methods.comparePassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateAccessToken = async function () {
+    return jwt.sign({
+        '_id': this._id,
+        'username': this.username,
+        'email': this.email,
+        'fullName': this.fullName,
+    }, process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: process.env.ACCESS_TOKEN_EXPIRY })
+}
+
+userSchema.methods.generateRefreshToken = async function () {
+    return jwt.sign({
+        '_id': this._id,
+    }, process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: process.env.REFRESH_TOKEN_EXPIRY })
+}
+
+const USER = mongoose.model('User', userSchema);
+
+export default USER;
