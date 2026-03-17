39df658273a64927c08185bc56ee6f6ba0e24a52
Soyam Paul <soyampaul34@gmail.com>
2026-03-17 12:39:40 +0530

    add subscription and user details retrieval functionality

--- src/controller/channel.controller.js
+++ src/controller/channel.controller.js
@@ -3,6 +3,7 @@ import APIError from "../utils/apiError.js";
 import APIResponse from "../utils/apiResponse.js";
 import asyncHandler from "../utils/asyncHandler.js";
 import USER from "../models/user.model.js";
+import mongoose from "mongoose";
 
 export const subscribeHandler = asyncHandler(async (req,res) =>{
     const userId = req.userId;
@@ -16,9 +17,6 @@ export const subscribeHandler = asyncHandler(async (req,res) =>{
 
     if(!channelId) throw new APIError(404,"Channel not found");
 
-    console.log(channelId._id);
-    console.log(userId);
-
     const check = await SUBSCRIBTION.findOne(
        {$and: [
         {subscriber:userId},
@@ -29,10 +27,54 @@ export const subscribeHandler = asyncHandler(async (req,res) =>{
     
     const response = await SUBSCRIBTION.create({
         subscriber:userId,
-        channel:channelId
+        channel:channelId._id
     });
 
     res.status(201).json(
         new APIResponse(201,response,"channel Subscribed")
     )
+})
+
+export const getUserSubsDetails = asyncHandler(async(req,res) =>{
+    const {user_id} = req.params;
+
+    const channelDetails = await USER.aggregate([
+        {$match:
+            {
+                _id: new mongoose.Types.ObjectId(user_id)
+
+            }
+        },
+        {
+            $lookup:{
+                from: 'subscribtions',
+                localField:'_id',
+                foreignField:'channel',
+                as:'subscribersData'
+            }
+        },
+        {
+            $lookup:{
+                from: 'subscribtions',
+                localField:'_id',
+                foreignField:'subscriber',
+                as:'subscriberedToData'
+            }
+        },
+        {
+            $addFields:{
+                subscriberCount:{
+                    $size: '$subscribersData'
+                },
+                subscribedToCount:{
+                    $size: '$subscriberedToData'
+                }
+            }
+        }
+    ])
+
+    if(!channelDetails.length) throw new APIError(404,"User not found");
+
+    console.log(channelDetails);
+    res.status(200).json(new APIResponse(200,channelDetails,"Usef found and sent"));
 })
--- src/routes/channel.routes.js
+++ src/routes/channel.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import {verifyJwt} from '../middlewares/verifyJwt.js'
-import {subscribeHandler} from '../controller/channel.controller.js'
+import {getUserSubsDetails, subscribeHandler} from '../controller/channel.controller.js'
 
 export const channelRouter = express.Router();
 
 channelRouter.use(verifyJwt);
 channelRouter.route("/subscribe").post(subscribeHandler);
+channelRouter.route("/c/:user_id").post(getUserSubsDetails);
 // channelRouter.route("/unsubscribe").post(unsubscribeHandler);
